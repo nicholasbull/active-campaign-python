adc0e983278e7c9c5f8619c4aeb6f8f93cc7c37a
Bart <contact@bartbania.com>
2015-09-25 21:26:19 +0100

    updated examples

--- examples.py
+++ examples.py
@@ -20,13 +20,44 @@ if __name__ == '__main__':
 
     # add custom list field
     list_field = {
-        'title': 'Custom Field',
+        'title': 'A Custom Field',
         'type': 1, # 1 = Text Field, 2 = Text Box, 3 = Checkbox, 4 = Radio, 5 = Dropdown, 6 = Hidden field, 7 = List Box, 9 = Date
         'req': 0,
-        'perstag': 'CUSTOM_F',
+        'perstag': 'CUSTOM_FIELD',
         'p[0]': 0
     }
     print ac.api('list/field_add', list_field)
+
+    # add message
+    message = {
+        'id': 12,
+        'format': 'html',
+        'subject': 'Test Message',
+        'fromemail': 'test@example.com',
+        'fromname': 'John Doe',
+        'priority': 1,
+        'charset': 'utf-8',
+        'htmlconstructor': 'external',
+        'htmlfetch': 'http://notify.bartbania.co.uk/notification.php?segment=1&email=%EMAIL%',
+        'htmlfetchwhen': 'send',
+        'p[1]': 1,
+    }
+    print ac.api('message/add', message)
+
+    # add campaign
+    campaign = {
+        'type': 'single',
+        'segmentid': 0,
+        'bounceid': 0,
+        'name': 'ActiveCampaign TEST',
+        'status': 1,
+        'tracklinks': 'none',
+        'trackreads': 1,
+        'htmlunsub': 1,
+        'p[1]': 1,
+        'm[12]': 100
+    }
+    print ac.api('campaign/create', campaign)
 
     # add subscriber
     subscriber = {
@@ -35,13 +66,13 @@ if __name__ == '__main__':
         'last_name': 'Smith',
         'p[1]': 1,
         'status[1]': 1,
-        'field[%CUSTOM_F%,0]': 'This is a field'
+        'field[%CUSTOM_FIELD%,0]': 'This is a field'
     }
     print ac.api('contact/sync', subscriber)
 
     # add note
     note = {
-        'id': 1,
+        'email': 'person@example.com',
         'listid': 1,
         'note': 'here is my note'
     }
@@ -53,3 +84,12 @@ if __name__ == '__main__':
         'tags': 'Tag1, Tag2, Tag3'
     }
     print ac.api('contact/tag_add', tag)
+
+    # campaign send
+    send = {
+        'email': 'person@example.com',
+        'campaignid': 1,
+        'messageid': 11,
+        'action': 'send'
+    }
+    print ac.api('campaign/send', send)
